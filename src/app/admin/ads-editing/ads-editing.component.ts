b45fce919a1bca205c2f5a67af01a69e20fbb600
Mikhail Ashla <redikfrost@gmail.com>
2022-03-26 21:21:04 +0300

    fix: solve problems of ads-editing and ads-moderation components

--- src/app/admin/ads-editing/ads-editing.component.ts
+++ src/app/admin/ads-editing/ads-editing.component.ts
@@ -1,72 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ad } from 'types/ad';
+import { AppDataService } from './../../services/app-data.service';
 
 @Component({
   selector: 'app-ads-editing',
   templateUrl: './ads-editing.component.html',
   styleUrls: ['./ads-editing.component.css']
 })
-export class AdsEditingComponent{
+export class AdsEditingComponent implements OnInit{
 
-  ads: Ad[] = [
-    {
-      pic: '',
-      title: 'House',
-      location: 'Minsk',
-      date: new Date(2022, 2, 4).toLocaleDateString(),
-      price: 100000
-    },
-    {
-      pic: '',
-      title: 'Villa',
-      location: 'Minsk',
-      date: new Date(2022, 5, 14).toLocaleDateString(),
-      price: 200000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
+  ads!: Ad[]
+
+  constructor(private appDataService: AppDataService) { }
+
+  ngOnInit(): void {
+    if (this.appDataService.adsData) {
+      this.ads = this.appDataService.adsData
+    } else {
+      this.appDataService.getAdsDataHTTP().subscribe(data => this.ads = Object.values(data))
     }
-  ]
+  }
 
-  constructor() { }
+  trackByFn(index: any, item: any) {
+    return index
+  }
 
+  chosenAdToEdit(ad: Ad): void {
+    this.appDataService.putChangeAdDataHTTP(ad)
+  }
 }
--- src/app/admin/ads-moderation/ads-moderation.component.ts
+++ src/app/admin/ads-moderation/ads-moderation.component.ts
@@ -1,75 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Ad } from 'types/ad';
+import { AppDataService } from './../../services/app-data.service';
 
 @Component({
   selector: 'app-ads-moderation',
   templateUrl: './ads-moderation.component.html',
   styleUrls: ['./ads-moderation.component.css']
 })
-export class AdsModerationComponent implements OnInit {
+export class AdsModerationComponent implements OnInit{
 
-  ads: Ad[] = [
-    {
-      pic: '',
-      title: 'House',
-      location: 'Minsk',
-      date: new Date(2022, 2, 4).toLocaleDateString(),
-      price: 100000
-    },
-    {
-      pic: '',
-      title: 'Villa',
-      location: 'Minsk',
-      date: new Date(2022, 5, 14).toLocaleDateString(),
-      price: 200000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    },
-    {
-      pic: '',
-      title: 'Penthouse',
-      location: 'Minsk',
-      date: new Date(2022, 3, 22).toLocaleDateString(),
-      price: 500000
-    }
-  ]
+  ads!: Ad[]
 
-  constructor() { }
+  constructor(private appDataService: AppDataService) { }
 
   ngOnInit(): void {
+    if (this.appDataService.adsData) {
+      this.ads = this.appDataService.adsData
+    } else {
+      this.appDataService.getAdsDataHTTP().subscribe(data => this.ads = Object.values(data))
+    }
   }
 
+  trackByFn(index: any, ad: any) {
+    return index
+  }
 }
