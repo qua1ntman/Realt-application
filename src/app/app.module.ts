fe125e6f0d9c8bdeb0c45bb981341affbc6ac0ac
Mikhail Ashla <redikfrost@gmail.com>
2022-03-26 21:17:11 +0300

    fix: transfer static data, update app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './authorization/sign-up/sign-up.component';
 import { SignInComponent } from './authorization/sign-in/sign-in.component';
-import { HttpClientModule }   from '@angular/common/http';
 import { AdminComponent } from './admin/admin.component';
-import { AdsModerationComponent } from './admin/ads-moderation/ads-moderation.component';
-import { AdsEditingComponent } from './admin/ads-editing/ads-editing.component';
-import { AppealsComponent } from './admin/appeals/appeals.component';
+
 import { CabineteComponent } from './client/cabinete/cabinete.component';
 import { EditAdPageComponent } from './client/edit-ad-page/edit-ad-page.component';
 import { CreateNewAdPageComponent } from './client/create-new-ad-page/create-new-ad-page.component';
 import { MapPageComponent } from './map-page/map-page.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { AuthorizationComponent } from './authorization/authorization.component';
 import { ClickStopPropaginationDirective } from './directives/stop-propagination/click-stop-propagination.directive';
-import { FormsModule } from '@angular/forms';
+import { ChosenAdPageComponent } from './chosen-ad-page/chosen-ad-page.component';
+import { AppDataService } from './services/app-data.service';
+import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
-
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @NgModule({
   declarations: [
@@ -28,9 +27,6 @@ import { MainComponent } from './main/main.component';
     SignUpComponent,
     SignInComponent,
     AdminComponent,
-    AdsModerationComponent,
-    AdsEditingComponent,
-    AppealsComponent,
     CabineteComponent,
     EditAdPageComponent,
     CreateNewAdPageComponent,
@@ -40,14 +36,10 @@ import { MainComponent } from './main/main.component';
     AuthorizationComponent,
     ClickStopPropaginationDirective,
     MainComponent,
+    ChosenAdPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule, NzSelectModule],
+  providers: [AppDataService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/constants.ts
@@ -0,0 +1,3 @@
+export const constants = {
+  baseApiUrl: 'http://localhost:3000/api/',
+};
